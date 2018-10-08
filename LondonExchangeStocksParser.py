309dbf9c8bcf8404d311e2836ac118988a1a9f96
Andrew <andre_zip@list.ru>
2018-10-08 21:53:34 +0300

    Разобратьс с запсью в файл

--- LondonExchangeStocksParser.py
+++ LondonExchangeStocksParser.py
@@ -17,7 +17,7 @@ def write_csv(data):
     with open("assets\LondonExchangeAnnual.csv", "a", newline="") as f:
         writer = csv.writer(f, delimiter=",")
 
-        writer.writerow(data)
+        writer.writerow([data])
 
 
 def read_links(path):
@@ -33,7 +33,7 @@ def read_links(path):
     return links
 
 def find_finance_page(url):
-    print("Finance URL: ", url)
+    # print("Finance URL: ", url)
     STR = "Fundamentals"
     html = get_html(url)
     bs = BeautifulSoup(html, "lxml")
@@ -51,38 +51,67 @@ def get_per_tax(url):
     print("Pre_tax URL: ", url)
     html = get_html(url)
     bs = BeautifulSoup(html, "lxml")
-    table = bs.find("table", class_ = "table_dati")
-    ths = table.find_all("th")
-    titles = []
-    for th in ths:
-        s = str(th.text)
-        print ("S: ", s)
-        newstr = s[:s.index(" ")]
-        #print(newstr)
-        titles.append(newstr)
-        print("Titles: ", titles)
+    try:
+        table = bs.find("table", class_ = "table_dati")
+        ths = table.find_all("th")
+
+        # Находим заголовки таблицы
+        titles = []
+        titles.append(url)
+        for th in ths:
+            s = str(th.text).strip()
+            # print ("S: ", s)
+            newstr = s[:s.index(" ")]
+            #print(newstr)
+            titles.append(newstr)
+        # print("Titles: ", titles)
+
+        # Находим данные нужной строки
+        data = []
+
+        trs = table.find("tbody").find_all("tr")
+        tr = trs[4]
+        tds = tr.find_all("td")
+        s = tds[0].text.strip()
+        # print("S: ", s)
+        if s == "Profit Before Tax":
+            # print("Find")
+            pre_tax = trs[4].find_all("td")
+            for t in pre_tax:
+                stripped = t.text.strip()
+                # print(stripped)
+                data.append(stripped)
+        else:
+            data.append("NO Profit Before Tax")
+        # print(data)
+    except:
+        data.append("NO Profit Before Tax")
+
+    titles.extend(data)
+    print(titles)
+    return titles
 
 
 def main():
     LIST_PATH = "assets\ListLondonExchange.csv"  # путь к списку
 
     # 1. Прочитать ссылки
-    #links = read_links(LIST_PATH)
+    links = read_links(LIST_PATH)
 
     # 2. Найти страничку с финансами
     data = []
-    #for link in links:
 
-    link = "https://www.londonstockexchange.com/exchange/prices-and-markets/stocks/summary/company-summary/GB00BCDBXK43GBGBXASX1.html"
+    #link = "https://www.londonstockexchange.com/exchange/prices-and-markets/stocks/summary/company-summary/GB00BCDBXK43GBGBXASX1.html"
 
-    pageURL = find_finance_page(link)
-    if pageURL is "":
-        data.append(link)
+    for link in links:
 
-    else:
-        per_tax = get_per_tax(pageURL)
-        data.append(per_tax)
-    write_csv(data)
+        pageURL = find_finance_page(link)
+        if pageURL is "":
+            data.append(link)
+        else:
+            per_tax = get_per_tax(pageURL)
+            data.append(per_tax)
+        write_csv(data)
 
 
 
