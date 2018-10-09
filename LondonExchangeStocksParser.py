7dc84fbb7d2fd3484f716823226cc1b244abe31d
Andrew <andre_zip@list.ru>
2018-10-09 17:25:38 +0300

    All work

--- LondonExchangeStocksParser.py
+++ LondonExchangeStocksParser.py
@@ -16,8 +16,7 @@ def get_html(url):
 def write_csv(data):
     with open("assets\LondonExchangeAnnual.csv", "a", newline="") as f:
         writer = csv.writer(f, delimiter=",")
-
-        writer.writerow([data])
+        writer.writerow(data)
 
 
 def read_links(path):
@@ -35,39 +34,48 @@ def read_links(path):
 def find_finance_page(url):
     # print("Finance URL: ", url)
     STR = "Fundamentals"
-    html = get_html(url)
-    bs = BeautifulSoup(html, "lxml")
-    refs = bs.find("div", class_ = "company_summary_tabs").find_all("a", class_ = "btn")
-    for ref in refs:
-        title = ref.get("title")
-        if title == STR:
-            a = "https://www.londonstockexchange.com" + ref.get("href")
-            return a
-        else:
-            continue
-    return " "
+    try:
+        html = get_html(url)
+        bs = BeautifulSoup(html, "lxml")
+        refs = bs.find("div", class_ = "company_summary_tabs").find_all("a", class_ = "btn")
+        for ref in refs:
+            title = ref.get("title")
+            if title == STR:
+                a = "https://www.londonstockexchange.com" + ref.get("href")
+                return a
+            else:
+                continue
+        return ""
+    except:
+        return ""
 
 def get_per_tax(url):
-    print("Pre_tax URL: ", url)
-    html = get_html(url)
-    bs = BeautifulSoup(html, "lxml")
+    # print("Pre_tax URL: ", url)
+    data = []
+    titles = []
     try:
+        html = get_html(url)
+        bs = BeautifulSoup(html, "lxml")
+
         table = bs.find("table", class_ = "table_dati")
         ths = table.find_all("th")
 
         # Находим заголовки таблицы
-        titles = []
-        titles.append(url)
+
         for th in ths:
             s = str(th.text).strip()
             # print ("S: ", s)
             newstr = s[:s.index(" ")]
             #print(newstr)
             titles.append(newstr)
+
+        # Добавляем пустых ячеек для выравнивания
+        while titles.__len__() < 9:
+            titles.append(" ")
+
         # print("Titles: ", titles)
 
         # Находим данные нужной строки
-        data = []
 
         trs = table.find("tbody").find_all("tr")
         tr = trs[4]
@@ -79,44 +87,39 @@ def get_per_tax(url):
             pre_tax = trs[4].find_all("td")
             for t in pre_tax:
                 stripped = t.text.strip()
-                # print(stripped)
+                #print("stripped ", stripped)
                 data.append(stripped)
         else:
-            data.append("NO Profit Before Tax")
+            data.append("NO Profit Per Tax")
         # print(data)
     except:
-        data.append("NO Profit Before Tax")
+        data.append("Exception Profit Per Tax")
 
     titles.extend(data)
-    print(titles)
+
     return titles
 
 
 def main():
     LIST_PATH = "assets\ListLondonExchange.csv"  # путь к списку
+    START_FROM = 435
 
-    # 1. Прочитать ссылки
-    links = read_links(LIST_PATH)
+    links = read_links(LIST_PATH)               # 1. Прочитать ссылки
 
-    # 2. Найти страничку с финансами
-    data = []
+    # тестовая ссылка link = "https://www.londonstockexchange.com/exchange/prices-and-markets/stocks/summary/company-summary/GB00BCDBXK43GBGBXASX1.html"
+    links = links[START_FROM:]
 
-    #link = "https://www.londonstockexchange.com/exchange/prices-and-markets/stocks/summary/company-summary/GB00BCDBXK43GBGBXASX1.html"
-
-    for link in links:
-
-        pageURL = find_finance_page(link)
+    for index, link in enumerate(links):
+        data = []
+        data.append(link)
+        pageURL = find_finance_page(link)       # 2. Найти страничку с финансами
         if pageURL is "":
-            data.append(link)
+            print(START_FROM + index, data)
         else:
-            per_tax = get_per_tax(pageURL)
-            data.append(per_tax)
-        write_csv(data)
-
-
-
-    # 3. Прочитать нужную строку
-    # 4. Записать в файл
+            per_tax = get_per_tax(pageURL)      # 3. Прочитать нужную строку
+            data.extend(per_tax)
+            print(START_FROM + index, data)
+        write_csv(data)                          # 4. Записать в файл
 
 
 #-----START---------
